de80cd42069f54bac047b52727d10e0040cbee18
Megan Brown <stockbtcrew@gmail.com>
2017-01-10 13:08:59 -0700

    fix constructor and take out varibales context and canvas

---
+++ test/card-test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var Card = require('../lib/card.js');
+console.log("test working");
+describe('Card', function () {
+  it('card is an object', function () {
+
+  })
+})
